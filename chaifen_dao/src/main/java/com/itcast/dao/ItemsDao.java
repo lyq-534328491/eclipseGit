b65a58e7c89e0c75084a975a0eedd3c842741248
lyq <lyq@qq.com>
2019-06-16 16:07:52 +0800

    添加了一个dao层的注解

--- chaifen_dao/src/main/java/com/itcast/dao/ItemsDao.java
+++ chaifen_dao/src/main/java/com/itcast/dao/ItemsDao.java
@@ -2,7 +2,9 @@ package com.itcast.dao;
 
 import com.itcast.domain.Items;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository //增加一个dao层的注解,完全不影响
 public interface ItemsDao {
 
     @Select("select * from items where id = #{id}")
