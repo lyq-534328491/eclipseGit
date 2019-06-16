9ece3c048b3e9b8673e7c6961fc4b447afab2e29
lyq <lyq@qq.com>
2019-06-16 15:57:16 +0800

    提交SSM整合拆分项目

---
+++ chaifen_dao/src/main/java/com/itcast/dao/ItemsDao.java
@@ -0,0 +1,10 @@
+package com.itcast.dao;
+
+import com.itcast.domain.Items;
+import org.apache.ibatis.annotations.Select;
+
+public interface ItemsDao {
+
+    @Select("select * from items where id = #{id}")
+    public Items findById(Integer id);
+}
